67668bc2e8f3f19a8042c2342ed05cf4d68e273c
Nick Adams <nick.pappas.adams@gmail.com>
2022-02-14 13:45:41 -0600

    first commit

---
+++ CsharpMaze/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpMaze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
